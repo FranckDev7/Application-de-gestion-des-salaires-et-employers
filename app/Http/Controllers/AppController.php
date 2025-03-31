6feec23ffe59e1418f93ca13d4044487d96c08bb
FranckDev7 <ffranckmuzabafr@gmail.com>
2025-03-31 20:04:44 +0100

    Afficher les statistiques réels sur le dashboard

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -3,11 +3,106 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Departement;
+use App\Models\Employer;
+use App\Models\User;
+
 
 class AppController extends Controller
 {
     public function index ()
     {
-        return view('dashboard');
+        $totalDepartements = Departement::all()->count();
+        $totalEmployers = Employer::all()->count();
+        $totalAdministrateurs = User::all()->count();
+        return view('dashboard', compact('totalDepartements', 'totalEmployers', 'totalAdministrateurs'));
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Portal - Bootstrap 5 Admin Dashboard Template For Developers</title>
+    <title>TP-Salaire laravel</title>
 
     <!-- Meta -->
     <meta charset="utf-8">
