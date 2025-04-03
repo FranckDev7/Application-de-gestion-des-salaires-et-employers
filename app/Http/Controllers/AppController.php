32b1fab8ac46de8d33086f13f46f608a4ae590df
FranckDev7 <ffranckmuzabafr@gmail.com>
2025-04-03 18:12:04 +0100

    Rappel de paiement sur le dashboard

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Models\Departement;
 use App\Models\Employer;
 use App\Models\User;
+use App\Models\Configuration;
+use Carbon\Carbon;
 
 
 class AppController extends Controller
@@ -17,15 +19,43 @@ class AppController extends Controller
      */
     public function index ()
     {
+        // Configuration de Carbon en français
+        Carbon::setLocale('fr');
 
+        // Récupération des totaux
         $totalDepartements = Departement::all()->count(); // Compte le nombre total de départements
-
         $totalEmployers = Employer::all()->count(); // Compte le nombre total d'employeurs
-
         $totalAdministrateurs = User::all()->count();  // Compte le nombre total d'administrateurs
 
-        // Retourne la vue du tableau de bord avec les totaux
-        return view('dashboard', compact('totalDepartements', 'totalEmployers', 'totalAdministrateurs'));
+        $defaultPaymentDate = null;
+        $paymentNotification = "";
+        $currentDate = Carbon::now()->day;
+
+        // Récupération du premier enregistrement dans la colonne 'type' ayant comme valeur 'PAYMENT_DATE'
+        $defaultPaymentDateQuery = Configuration::where('type', 'PAYMENT_DATE')->first();
+
+        if ($defaultPaymentDateQuery) {
+            // Recuperation de la valeur de la colonne 'value'
+            $defaultPaymentDate = $defaultPaymentDateQuery->value;
+            $convertedPaymentDate = intval($defaultPaymentDate);
+
+            if ($currentDate < $convertedPaymentDate) {
+                $paymentNotification = "Le paiement doit avoir lieu le {$defaultPaymentDate} de ce mois.";
+            } else {
+                $nextMonth = Carbon::now()->addMonth();
+                $nextMonthName = ucfirst($nextMonth->translatedFormat('F')); // Mois en français
+                $paymentNotification = "Le paiement doit avoir lieu le {$defaultPaymentDate} du mois de {$nextMonthName}";
+            }
+
+
+        }
+
+        // Retourne la vue du tableau de bord avec les variables
+        return view('dashboard', compact('totalDepartements', 'totalEmployers', 'totalAdministrateurs', 'paymentNotification'));
+
+
+
+
     }
 }
 
